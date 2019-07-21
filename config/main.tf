6a93672bb98ef93f3664b934ae7e49151522522c
Anh Vo <anhvoo.dev@gmail.com>
2019-07-20 23:03:11 -0700

    Reorganize objects by type

--- config/main.tf
+++ config/main.tf
@@ -18,6 +18,31 @@ resource "aws_launch_configuration" "example" {
   }
 }
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids 
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch =  true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -32,32 +57,9 @@ variable "server_port" {
   default     = 8080
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port 
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP address of the web server"
 }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnet_ids.default.ids 
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch =  true
-  }
-}
