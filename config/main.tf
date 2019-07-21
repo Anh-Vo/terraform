6ac57a21b73a59133a8b49d8af3361675401192b
Anh Vo <anhvoo.dev@gmail.com>
2019-07-20 23:25:29 -0700

    Created Application Load Balancer (ASG) along with a security group to allow ingress and egress out of AWS. Also created a load balancer target group and assign the load balancer to route traffic with the correct request endpoints to the target group. Additionally, added health check that looks for a 200 return on the instances in the Auto Scaling Group (ASG) to make sure traffic isn't routed to inactive nodes.

--- config/main.tf
+++ config/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_launch_configuration" "example" {
-  image_id           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
@@ -20,7 +20,10 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnet_ids.default.ids 
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids 
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -40,6 +43,67 @@ resource "aws_security_group" "instance" {
     to_port     = var.server_port 
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fix_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP" 
+  vpc_id   = data.aws_vpc.default.id
+  
+  health_check {
+    path = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
   }
 }
 
